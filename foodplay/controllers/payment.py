c0fa97182dab25c1b01fcd59a3ae2365ca3d26cc
pengfei <andysim3d@gmail.com>
2016-05-17 16:13:07 -0400

    almost finish first part

--- foodplay/controllers/payment.py
+++ foodplay/controllers/payment.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.template.loader import get_template
 from django.views.generic import TemplateView
@@ -13,8 +13,8 @@ class Pay(TemplateView):
         pass
         temp = get_template("pay.html")
         paypalform = PaypalForm()
-        html = temp.render(RequestContext({'form': paypalform}))
-        return HttpResponse(html)
+        html = RequestContext({'form': paypalform})
+        return render_to_response("pay.html", html)
 
 
         # def get(self, request,*args, **kwargs):
