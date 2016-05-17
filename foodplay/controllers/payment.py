6ce0d3b85d23a8d1b534fd7e20207895e6b6b3f8
andysim3d <andysim3d@gmail.com>
2016-05-17 16:45:14 -0400

    almost finish first part

--- foodplay/controllers/payment.py
+++ foodplay/controllers/payment.py
@@ -2,7 +2,6 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.template.context_processors import csrf
 from django.template.loader import get_template
-from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.generic import TemplateView
 
 from foodplay.forms.paymentform import PaypalForm
@@ -11,7 +10,6 @@ from foodplay.forms.paymentform import PaypalForm
 class Pay(TemplateView):
     template_name = 'pay.html'
 
-    @ensure_csrf_cookie
     def get(self, request):
         pass
         c = {}
@@ -19,10 +17,18 @@ class Pay(TemplateView):
 
         temp = get_template("pay.html")
         paypalform = PaypalForm()
+
         html = RequestContext(request, {'form': paypalform}, c)
-        return render_to_response("pay.html", html)
+        return render_to_response("pay.html", c)
 
 
         # def get(self, request,*args, **kwargs):
         #     context = self.get_context_data(kwargs)
         #     return context
+
+    def post(self, request):
+        temp = get_template("pay.html")
+        paypalform = PaypalForm()
+
+        html = RequestContext(request, {'form': paypalform, "info": "succeed!!!"})
+        return render_to_response("pay.html", html)
