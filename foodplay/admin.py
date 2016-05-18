44f913cb44628242af15c35faba671fe985bf3c4
andysim3d <andysim3d@gmail.com>
2016-05-18 15:16:36 -0400

    almost finish first part

--- foodplay/admin.py
+++ foodplay/admin.py
@@ -4,5 +4,5 @@ from django.contrib import admin
 
 from models import *
 
-admin.site.register(Items)
-admin.site.register(Payhistory)
+admin.site.register(Products)
+admin.site.register(CardInformations)
