1ca62edd7ae9bb8e35492822b775505196d37423
pengfei <andysim3d@gmail.com?>
2016-05-18 20:56:11 -0400

    work with challenges

--- foodplay/forms/paypalsubmit.py
+++ foodplay/forms/paypalsubmit.py
@@ -42,12 +42,15 @@ class paypal(object):
     def convertpaymentinfo(payment_info, item_info):
         pinfo = dict()
         try:
+            card_info = model_to_dict(payment_info)
+            del card_info['item']
+            del card_info['id']
             pinfo['intent'] = ("sale")
             pinfo['payer'] = {}
             pinfo['payer']['payment_method'] = 'credit_card'
             pinfo['payer']['funding_instruments'] = [{"credit_card": model_to_dict(payment_info)}]
 
-            pinfo['transactions'] = [{"item_list": item_info}]
+            pinfo['transactions'] = [item_info]
         except Exception as E:
             raise E
         return pinfo
@@ -58,7 +61,49 @@ class paypal(object):
         # items['quantity'] = 1
         # items['currency'] = "USD"
         item_info = dict()
+
         # item_info['items'] = [items]
-        item_info['amount'] = {'total': items.get("price", 1), 'currency': "USD"}
+        item_info['amount'] = {'total': int(items.get("price", 1)), 'currency': "USD"}
         item_info["description"] = items["description"]
         return item_info
+
+
+#
+d = {
+        'intent': 'sale',
+          'payer': {
+              'funding_instruments': [{
+                      'credit_card': {
+                          'cvv2': 874,
+                          'expire_month': 11,
+                          'expire_year': 2018,
+                          'first_name': 'Joe',
+                          'last_name': 'Shopper',
+                          'number': '4446283280247004',
+                          'type': 'visa'}
+        }],
+              'payment_method': 'credit_card'},
+ 'transactions': [{
+        'amount': {   'currency': 'USD',
+                      'total': 12},
+        'description': 'popcake decoration kit'}]}
+
+
+# {
+#   "intent": "sale",
+#   "payer": {
+#     "payment_method": "credit_card",
+#     "funding_instruments": [{
+#       "credit_card":{
+#         "type": "visa",
+#         "number": "4446283280247004",
+#         "expire_month": "11",
+#         "expire_year": "2018",
+#         "cvv2": "874",
+#         "first_name": "Joe",
+#         "last_name": "Shopper" }}]},
+#   "transactions": [{
+#     "amount": {
+#       "total": "12",
+#       "currency": "USD" },
+#     "description": "creating a direct payment with credit card" }]}
